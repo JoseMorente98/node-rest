d09e90114ad76fc522e91cf539063dbe9bee90c9
José Rafael Morente González <josemorenteg98@gmail.com>
2020-01-09 12:17:23 -0600

    [UPDATED]

--- src/router/hola.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import HolaController from "./../controller/hola.controller"
-const foro = Router();
-
-foro.get('/saludar', HolaController.getInstance().getAll);
-
-export default foro;
