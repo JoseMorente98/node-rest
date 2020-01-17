22db38899c56144f3996697628d435b7539db5b6
José Rafael Morente González <josemorenteg98@gmail.com>
2020-01-17 13:48:45 -0600

    [UPDATED]

---
+++ server/controller/usuario.controller.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mysql_1 = __importDefault(require("./../mysql/mysql"));
+var UsuarioController = /** @class */ (function () {
+    function UsuarioController() {
+        this.getAll = function (req, res) {
+            var query = "SELECT * FROM `users`";
+            mysql_1.default.getQuery(query, function (err, data) {
+                if (err) {
+                    res.json([]);
+                }
+                else {
+                    res.json(data);
+                }
+            });
+        };
+    }
+    UsuarioController.getInstance = function () {
+        return this._instance || (this._instance = new this());
+    };
+    return UsuarioController;
+}());
+exports.default = UsuarioController;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,40 @@
 require('./config/config.js');
 const express = require('express');
 const app = express();
+var api = "/api/";
+var bodyParser = require("body-parser");
+
+var usuario_router_1 = require("./routes/usuario.router");
+var mysql_1 = require("./mysql/mysql");
+
+mysql_1.default.getInstance();
 
 app.get('/', function (req, res) {
   res.json('Hello World!');
 });
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  if (req.methods == "OPTIONS") {
+      res.sendStatus(200);
+  }
+  else {
+      next();
+  }
+});
+
+/**
+ * BODY PARSER
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * ROUTER
+ */
+app.use(api, usuario_router_1.default);
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port! ', process.env.PORT);
